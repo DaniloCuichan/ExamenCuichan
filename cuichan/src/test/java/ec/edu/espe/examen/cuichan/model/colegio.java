7ed56f144e7c3d98f045aaab3fad6524af1cf0cf
DaniloCuichan <65546976+DaniloCuichan@users.noreply.github.com>
2023-12-20 21:07:53 -0500

    2
    2

--- cuichan/src/test/java/ec/edu/espe/examen/cuichan/model/colegio.java
+++ cuichan/src/test/java/ec/edu/espe/examen/cuichan/model/colegio.java
@@ -1,5 +1,34 @@
 package ec.edu.espe.examen.cuichan.model;
 
-public class colegio {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+    @Entity
+    @Table(name = "COLEGIO")
+    @Data
+    @Builder
+
+
+public class colegio implements Serializable  {
+
+    @Id
+     @Column(name = "COD_COLEGIO", nullable = false)
+     private Integer codColegio;
+     @Column(name = "NOMBRE", length = 100, nullable = false)
+    private String nombre;
+    public colegio() {
+    }
+    public colegio(Integer codColegio, String nombre) {
+        this.codColegio = codColegio;
+        this.nombre = nombre;
+    }
+
+    
 
 }
